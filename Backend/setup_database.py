0e28dfaf380010d6782fe68511a2977480700927
insane-banda <piyushkeshari24@gmail.com>
2020-12-09 00:14:27 +0530

    Modified to add functionality to populate tables

--- Backend/setup_database.py
+++ Backend/setup_database.py
@@ -2,7 +2,7 @@ import mysql.connector
 import os
 import re
 
-c = [re.findall(r".*='(.*)'.?", line) for line in open('.\config.txt')]
+c = [re.findall(r".*='(.*)'.?", line) for line in open('config.txt')]
 h = c[0][0]
 u = c[1][0]
 p = c[2][0]
@@ -13,14 +13,14 @@ def create_schema():
     mycursor = mydb.cursor()
     mycursor.execute("DROP SCHEMA IF EXISTS ums;")
     f = open("./SQL Scripts/Create Schema/Schema.sql")
-    mycursor.execute(f.read())
+    mycursor.execute(f.read(), multi=True)
     f.close()
     print("Successfully created schema.......:)\n")
     mycursor.close()
 
 
-non_dependent_entity = ["Students", "Courses",
-                        "Clubs", "Departments", "Login"]
+non_dependent_entity = ["Students", "Courses", "Clubs", "Departments"]
+
 dependent_entity = [script for script in [x.replace(".sql", "") for x in os.listdir(
     "./SQL Scripts/Create Tables")] if script not in non_dependent_entity]
 
@@ -29,41 +29,45 @@ dependent_entity.sort()
 
 
 def create_non_dependent_tables():
-    mydb = mysql.connector.connect(
-        host=h, user=u, password=p, database="ums")
-    mycursor = mydb.cursor()
     for script in non_dependent_entity:
         with open("./SQL Scripts/Create Tables/"+script+".sql") as f:
             print(f"Creating {script} table...")
-            mycursor.execute(f.read())
+            for line in f.readlines():
+                mydb = mysql.connector.connect(
+                    host=h, user=u, password=p, database="ums")
+                mycursor = mydb.cursor()
+                mycursor.execute(line)
+                mydb.commit()
+                mycursor.close()
             print(f"successfully created {script} table.......:)\n")
-    mycursor.close()
     print("Completed successfully\n")
 
 
 def create_dependent_tables():
-    mydb = mysql.connector.connect(
-        host=h, user=u, password=p, database="ums")
-    mycursor = mydb.cursor()
     for script in dependent_entity:
         with open("./SQL Scripts/Create Tables/"+script+".sql") as f:
             print(f"Creating {script} table...")
-            mydb.cursor().execute(f.read())
+            for line in f.readlines():
+                mydb = mysql.connector.connect(
+                    host=h, user=u, password=p, database="ums")
+                mycursor = mydb.cursor()
+                mycursor.execute(line)
+                mydb.commit()
+                mycursor.close()
             print(f"successfully created {script} table.......:)\n")
-    mycursor.close()
     print("Completed successfully\n")
 
 
 def create_relation_tables():
-    mydb = mysql.connector.connect(
-        host=h, user=u, password=p, database="ums")
-    mycursor = mydb.cursor()
     for script in relations:
+        mydb = mysql.connector.connect(
+            host=h, user=u, password=p, database="ums")
+        mycursor = mydb.cursor()
+        print(f"Creating {script} table...")
         with open("./SQL Scripts/Create Relationships/"+script) as f:
-            print(f"Creating {script} table...")
-            mydb.cursor().execute(f.read())
-            print(f"successfully created {script} table.......:)\n")
-    mycursor.close()
+            mycursor.execute(f.read(), multi=True)
+        print(f"successfully created {script} table.......:)\n")
+        mycursor.close()
     print("Completed successfully\n")
 
 
