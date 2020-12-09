2e67b26f244b8dd335d961215c86e41e80bc5cc7
insane-banda <piyushkeshari24@gmail.com>
2020-12-09 22:44:03 +0530

    Updated few tables

--- SQL Scripts/Create Tables/Faculties.sql
+++ SQL Scripts/Create Tables/Faculties.sql
@@ -1,11 +1,6 @@
 CREATE TABLE FACULTIES(Emp_ID NUMERIC(6),Experience NUMERIC(2),Course_ID NUMERIC(6),FOREIGN KEY (Emp_ID) REFERENCES EMPLOYEES(Emp_ID),FOREIGN KEY (Course_ID) REFERENCES COURSES(Course_ID));
-insert into FACULTIES values(145623,5,15523);
 insert into FACULTIES values(221534,3,23214);
 insert into FACULTIES values(325475,2,34785);
 insert into FACULTIES values(451326,6,45687);
 insert into FACULTIES values(565767,2,56799);
-insert into FACULTIES values(625648,3,67815);
-insert into FACULTIES values(715689,7,78915);
-insert into FACULTIES values(988947,4,98798);
-insert into FACULTIES values(111346,2,98798);
 insert into FACULTIES values(768565,5,78915);
--- SQL Scripts/Create Tables/Trainers.sql
+++ SQL Scripts/Create Tables/Trainers.sql
@@ -1,11 +1,5 @@
 CREATE TABLE TRAINERS(Emp_ID NUMERIC(6), Specialization VARCHAR(255), FOREIGN KEY (Emp_ID) REFERENCES EMPLOYEES(Emp_ID));
 insert into TRAINERS values(145623,'Computer Science');
 insert into TRAINERS values(221534,'Cloud Computing');
-insert into TRAINERS values(325475,'Cyber Security');
-insert into TRAINERS values(451326,'Artificial Intelligence');
-insert into TRAINERS values(565767,'Coding Technology');
-insert into TRAINERS values(625648,'Robotics');
-insert into TRAINERS values(715689,'Automobile Engineering');
-insert into TRAINERS values(988947,'Biotechnology Engineering');
 insert into TRAINERS values(111346,'Computer Science');
 insert into TRAINERS values(768565,'Robotics');
